3a7b7efc32e361d2347057cbe3c8997f44135522
Graham Baradoy <graham@baradoy.ca>
2021-04-17 10:12:06 -0700

    Add Accounts context and User schema

--- lib/intermediate_git_web.ex
+++ lib/intermediate_git_web.ex
@@ -83,6 +83,7 @@ defmodule IntermediateGitWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import IntermediateGitWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
